f7f549cf56ba135e87825c77ba8f10bdb15d1a48
Brandon Turner <bcturner@gmail.com>
2019-12-04 23:30:04 -0500

    Added Project files

---
+++ dvdScreensaver/dvdScreensaverView.swift
@@ -0,0 +1,91 @@
+
+import ScreenSaver
+
+class dvdScreensaverView: ScreenSaverView {
+    
+    private var dvdPosition: CGPoint = .zero
+    private var dvdVelocity: CGVector = .zero
+    private let dvdHeight: CGFloat = 173.5
+    private let dvdWidth: CGFloat = 220
+    var image: NSImage?
+    
+    // MARK: - Initialization
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
+        dvdPosition = CGPoint(x: frame.width / 2, y: frame.height / 2)
+        dvdVelocity = initialVelocity()
+        loadImage();
+    }
+    
+    private func initialVelocity() -> CGVector {
+        let desiredVelocityMagnitude: CGFloat = 10
+        let xVelocity = CGFloat.random(in: 2.5...7.5)
+        let xSign: CGFloat = Bool.random() ? 1 : -1
+        let yVelocity = sqrt(pow(desiredVelocityMagnitude, 2) - pow(xVelocity, 2))
+        let ySign: CGFloat = Bool.random() ? 1 : -1
+        return CGVector(dx: xVelocity * xSign, dy: yVelocity * ySign)
+    }
+    
+
+    private func dvdIsOOB() -> (xAxis: Bool, yAxis: Bool) {
+        let xAxisOOB = dvdPosition.x <= 0 ||
+            dvdPosition.x + (dvdWidth / 2) >= bounds.width
+        let yAxisOOB = dvdPosition.y <= 0 ||
+            dvdPosition.y + (dvdHeight / 2) >= bounds.height
+        return (xAxisOOB, yAxisOOB)
+    }
+
+    @available(*, unavailable)
+    required init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Lifecycle
+    override func draw(_ rect: NSRect) {
+        drawBackground(.black)
+        drawDVD()
+    }
+    
+    func loadImage() {
+        DispatchQueue.global().async() {
+            let url = NSURL(string: "https://img.itch.zone/aW1nLzE3MTE3MjcucG5n/original/rySJh4.png")
+            let data = NSData(contentsOf: url! as URL)
+            if let data = data {
+                self.image = NSImage(data: data as Data)
+                self.needsDisplay = true
+            }
+        }
+    }
+    
+    private func drawDVD() {
+        if let image = image {
+            let point = CGPoint(x: dvdPosition.x, y: dvdPosition.y)
+            image.draw(at: point, from: NSZeroRect, operation: NSCompositingOperation.sourceOver, fraction: 1)
+        }
+
+    }
+    
+    private func drawBackground(_ color: NSColor) {
+        let background = NSBezierPath(rect: bounds)
+        color.setFill()
+        background.fill()
+    }
+
+    override func animateOneFrame() {
+        super.animateOneFrame()
+
+        let oobAxes = dvdIsOOB()
+        if oobAxes.xAxis {
+            dvdVelocity.dx *= -1
+        }
+        if oobAxes.yAxis {
+            dvdVelocity.dy *= -1
+        }
+        
+        dvdPosition.x += dvdVelocity.dx
+        dvdPosition.y += dvdVelocity.dy
+        
+        setNeedsDisplay(bounds)
+    }
+
+}
